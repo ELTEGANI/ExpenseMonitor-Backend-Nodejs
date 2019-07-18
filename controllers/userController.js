065cc64d409539073d75fb0aaa97ba2ca102e342
tigani <tiganimoh@gmail.com>
2019-07-18 22:02:16 +0200

    user can register,createexpnese and retreive all expenses with authentications

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const {validationResult} = require('express-validator/check');
 const Users = require('../models/users');
 const userExpenses = require('../models/userExpenses');
+const jwt = require('jsonwebtoken');
 
 
 exports.signUpUser = (req,res,next) => {
@@ -15,14 +16,21 @@ exports.signUpUser = (req,res,next) => {
     const emailaddress   = req.body.emailaddress;
     const gender         = req.body.gender;
 
-    Users.create({
+    Users.findOne({where:{emailaddress:emailaddress}})
+      .then(user=>{
+         if(user){
+            const token = jwt.sign({userId:user.id},'expensemon');
+            res.status(200).json({accesstoken:token,userid:user.id})
+         }else{
+        Users.create({
             username:username,
             emailaddress:emailaddress,
             gender:gender
             }).then(result =>{
+            const token = jwt.sign({userId:result.id},'expensemon');
             res.status(201).json({
-             message:'User Created Successfully',
-             User:result
+            accesstoken:token,
+            userid:userId
          });
          })
         .catch(
@@ -33,6 +41,15 @@ exports.signUpUser = (req,res,next) => {
                 next(err);
              }
         )  
+         }
+      }).catch(err =>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+       })
+
+   
 }
 
 
@@ -48,15 +65,14 @@ exports.createExpense = (req,res,next) => {
     const amount       = req.body.amount;
     const note         = req.body.note;
     const date         = req.body.date;
-    const UserId       = req.body.userid;
     const expenseCategory  = req.body.category;
     const expesnseFrom      = req.body.form;
-
+   
     userExpenses.create({
             amount:amount,
             note:note,
             date:date,
-            UserId:UserId,
+            UserId:req.userId,
             expenseCategory:expenseCategory,
             expesnseFrom:expesnseFrom
             }).then(result =>{
@@ -74,3 +90,34 @@ exports.createExpense = (req,res,next) => {
              }
         )  
 }
+
+
+exports.getAllExpenses=(req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+       const error = new Error('validation Failed');
+       error.statusCode = 422;
+       error.data = errors.array();
+       throw error
+    }
+    const userId            = req.userId;
+    console.log(userId)
+    userExpenses.findAll({
+        attributes: ['amount','note','expenseCategory','expesnseFrom','date'],
+        where:{userId:userId}
+    })
+    .then(expenses=>{
+        res.status(201).json({
+            message:'My Expenses',
+            myExpenses:expenses,
+      });
+    }).catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+    
+ }
+
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {body} = require('express-validator/check');
 const userController = require('../controllers/userController');
 const router = express.Router();
+const isAuth = require('../authenticationMiddleware/middleware');
+
 
 router.post('/registeruser',[
         body('username').trim().not().isEmpty(),
@@ -9,15 +11,15 @@ router.post('/registeruser',[
         body('gender').trim().not().isEmpty()   
        ],userController.signUpUser);
 
-router.post('/createexpense',[
+router.post('/createexpense',isAuth,[
         body('amount').trim().not().isEmpty(),
         body('note').trim(),
         body('date').trim().not().isEmpty(),
-        body('userid').trim().not().isEmpty(),
         body('category').trim().not().isEmpty(),
         body('form').trim().not().isEmpty()   
        ],userController.createExpense);
 
 
-
+router.get('/retreiveallexpense',isAuth,userController.getAllExpenses)
+       
 module.exports = router;
