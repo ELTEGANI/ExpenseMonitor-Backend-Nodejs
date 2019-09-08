2466cd6ca01e327506bf0d20d0d9939a4c4cd980
EL Tegani <etegani@localhost.localdomain>
2019-09-08 08:35:31 +0200

    adapted in fedora

--- config/configration.js
+++ config/configration.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ExpenseMonitor','root','root123', { 
+const sequelize = new Sequelize('ExpenseMonitor','root','AdminAdminAdmin_123', { 
     operatorsAliases: false ,
     dialect: 'mysql',
     host:'localhost'
 
 });
-  
+    
 module.exports = sequelize;
 
 
