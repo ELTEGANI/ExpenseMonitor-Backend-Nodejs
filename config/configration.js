be66d20e9401ffefd50657089f66cad028c8c77c
root <root@localhost.localdomain>
2019-10-12 11:22:33 +0200

    added configuration to backend

--- config/configration.js
+++ config/configration.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.DATABASE_USER}`,
 `${process.env.DATABASE_PASSWORD}`, { 
-    // operatorsAliases: false ,
+    operatorsAliases: false ,
     dialect: 'mysql',
-    host:'eu-cdbr-west-02.cleardb.net/heroku_8c5f8d5377ca51a?reconnect=true'
+    host:'localhost'
 
 });
     
