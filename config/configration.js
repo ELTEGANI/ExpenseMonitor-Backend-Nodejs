612adee7bc173763e2709c6de9e8803b6c735a4b
root <root@localhost.localdomain>
2019-10-12 16:22:06 +0200

    added localgost to config

--- config/configration.js
+++ config/configration.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('ExpenseMonitor','root','MySqlAdminAdminAdmin_123',{ 
     dialect: 'mysql',
-    host:'http://104.207.131.106'
+    host:'localhost'
 
 });
     
