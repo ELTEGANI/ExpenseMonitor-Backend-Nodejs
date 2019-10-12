5678c48153013a53ca04a4550f8edfe101b976d5
root <root@localhost.localdomain>
2019-10-12 13:17:16 +0200

    removed operatorsAliases

--- config/configration.js
+++ config/configration.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(`${process.env.DATABASE_NAME}`,`${process.env.DATABASE_USER}`,
 `${process.env.DATABASE_PASSWORD}`, { 
-    operatorsAliases: false ,
     dialect: 'mysql',
     host:'localhost'
 
