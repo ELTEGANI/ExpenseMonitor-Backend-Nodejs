6d41b30e85189835221fb12c7516e6d8aafb2246
root <root@localhost.localdomain>
2019-10-13 14:07:31 +0200

    added online config

--- config/configration.js
+++ config/configration.js
@@ -1,18 +1,18 @@
 const Sequelize = require('sequelize');
 
 //online server
-// const sequelize = new Sequelize('ExpenseMonitor','root','MySqlAdminAdminAdmin_123',{ 
-//     dialect: 'mysql',
-//     host:'localhost'
-
-// });
-  
-//localserver
-const sequelize = new Sequelize('ExpenseMonitor','root','AdminAdminAdmin_123',{ 
+const sequelize = new Sequelize('ExpenseMonitor','root','MySqlAdminAdminAdmin_123',{ 
     dialect: 'mysql',
     host:'localhost'
 
-})
+});
+  
+//localserver
+// const sequelize = new Sequelize('ExpenseMonitor','root','AdminAdminAdmin_123',{ 
+//     dialect: 'mysql',
+//     host:'localhost'
+
+// })
 module.exports = sequelize;
 
 
